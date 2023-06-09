14f2d33b07fca29df770e52e36205d17996916e4
Kanushka Gayan <kanushkanet@gmail.com>
2023-06-10 00:37:13 +0530

    FIx image scalling issue

--- components/ServiceCard.vue
+++ components/ServiceCard.vue
@@ -32,7 +32,7 @@
           </div>
         </div>
         <img v-if="hasImage" :src="service.image.src" :alt="service.image.alt"
-          class="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
+          class="w-[48rem] lg:max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
           width="2432" height="1442" />
         <video v-if="hasVideo" controls>
           <source :src="service.video.src" type="video/mp4">
