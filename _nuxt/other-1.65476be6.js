d5a5c6e2fb350dacac85185d00f94ede4a901267
Kanushka Gayan <kanushkanet@gmail.com>
2023-06-18 19:19:33 +0530

    Updates

---
+++ _nuxt/other-1.65476be6.js
@@ -0,0 +1,1 @@
+import"./entry.6f926fd0.js";const e=""+new URL("other-1.934dff88.jpg",import.meta.url).href;export{e as _};
--- _nuxt/other-1.9a7b4acd.js
+++
@@ -1,1 +0,0 @@
-import"./entry.8ad6c849.js";const e=""+new URL("other-1.934dff88.jpg",import.meta.url).href;export{e as _};
